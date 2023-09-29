ea08c3183e603f3cae0a8d0d93db3a619816ab08
Nicoll2104 <soda210461@gmail.com>
2023-09-29 17:42:35 -0500

    primero

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="todo">
     <div class="arriba">
       <div class="logo">
-      <img src="/src/assets/img.png" alt="" id="logo">
+      <img src="/src/assets/logo.png" alt="" id="logo">
     </div>
     <div class="barra">
     <input type="text" placeholder=" 🔍Buscar" id="barra1" v-model="txtBuscar">
@@ -19,9 +19,10 @@
 <div class="collapse" id="collapseExample" >
   <div class="card card-body" id="opciones">
     <div v-for="(opcion, index) in opciones" :key="index" id="opcion1">
-      <input type="radio" :id="'opcion' + (index + 1)" :name="'grupoOpciones'" :value="opcion" v-model="opcionSeleccionada">
+      <input type="radio" :id="'opcion' + (index + 1)" :name="'grupoOpciones'" :value="opcion" @click="filtrar(opcion)" :checked="radio">
       <label :for="'opcion' + (index + 1)">{{ opcion }}</label><br>
     </div>
+    <button @click="quitarFiltro" class="quitar">Quitar</button>
   </div>
 </div>
   </div>
@@ -35,7 +36,7 @@
       </div>
     </div>
   </div>
-  <div class=" card-grid" v-if="!componenteBuscar">
+  <div class=" card-grid" v-if="!componenteBuscar && !componenteNuevo">
     <div v-for="pokemon in todo" :key="pokemon.id" class="card">
       <img :src="pokemon.img" class="card-img-top" alt="..." data-bs-toggle="modal" :data-bs-target="'#exampleModal' + pokemon.id">
       <div class="card-body">
@@ -46,6 +47,18 @@
     </div>
     <div class="ultimo">
     <button @click="mostrar()" id="ultimo"> Ver más</button>
+  </div>
+  </div>
+  <div class=" card-grid" v-if="componenteNuevo">
+    <div v-for="pokemon in nuevo" :key="pokemon.id" class="card">
+      <img :src="pokemon.img" class="card-img-top" alt="..." data-bs-toggle="modal" :data-bs-target="'#exampleModal' + pokemon.id">
+      <div class="card-body">
+        <h5 class="card-title">N°{{ pokemon.id }}</h5>
+        <h1 class="card-text">{{ pokemon.nombre }}</h1>
+        <p class="tipo" v-for="tipo in pokemon.tipos" :style="'background-color:' + colores[tipo]">{{ tipo }}</p>
+      </div>
+    </div>
+    <div class="ultimo">
   </div>
   </div>
   <div>
@@ -59,10 +72,16 @@
             <div class="modal-body">
               <h1 class="id-modal">#{{ pokemon.id }}</h1>
               <h1 class="nombre-modal">{{ pokemon.nombre }}</h1>
+              <div class="lado">
               <img :src="pokemon.img" alt="" class="img-fluid" />
+              <div class="lado1">
               <p class="tipo" id="tipo-modal" v-for="tipo in pokemon.tipos" :style="'background-color:' + colores[tipo]">{{ tipo }}</p>
+              <div class="dos">
               <p class="igual-modal"><span class="bold-text">Altura:</span> {{ pokemon.altura }}</p>
               <p class="igual-modal"><span class="bold-text">Peso:</span> {{ pokemon.peso }}</p>
+            </div>
+          </div>
+          </div>
               <h2>Estadísticas:</h2>
               <div v-for="stat in pokemon.estadisticas" :key="stat.name">
                 <div class="stat-container">
@@ -105,7 +124,8 @@ const colores = {
   rock: "#b8b6ad",
   steel: "#a6cad4",
   ice:"#e7eff6",
-  ghost:"#8f728e"
+  ghost:"#8f728e",
+ /*  dragon */
 };
 const componenteBuscar = ref(false); 
 const opciones = ['grass','poison','fire','flying','water','bug','normal','electric','ground','fairy','fighting','psychic','rock','steel','ice','ghost'];
@@ -142,11 +162,75 @@ async function obtenerUrlsPokemon(i) {
 const txtBuscar = ref("")
 const buscado = ref({})
 
-function buscar(){
-  buscado.value = todo.value.find(s =>s.nombre == txtBuscar.value)
-  componenteBuscar.value = true
+async function buscar() {
+  let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${txtBuscar.value.toLowerCase()}`);
+  
+  let pokemonData = response.data;
 
+  buscado.value = {
+    id: pokemonData.id,
+    img: pokemonData.sprites.other["official-artwork"].front_default,
+    nombre: pokemonData.name,
+    altura: pokemonData.height,
+    peso: pokemonData.weight,
+    tipos: pokemonData.types.map((tipo) => tipo.type.name),
+    estadisticas: pokemonData.stats.map((stat) => {
+      return { name: stat.stat.name, cant: stat.base_stat };
+    }),
+  };
+
+  componenteBuscar.value = true;
 }
+
+
+const opcionSeleccionada = ref("")
+const nuevo = ref([])
+
+const componenteNuevo = ref(false)
+
+async function filtrar(opcion) {
+  nuevo.value = []; // Limpiar el array antes de agregar nuevos Pokémon
+
+  // Obtener datos de la API de tipo de Pokémon
+  let response = await axios.get(`https://pokeapi.co/api/v2/type/${opcion}`);
+  
+  // Obtener las URLs de los Pokémon del tipo específico
+  let pokemonUrls = response.data.pokemon.map((poke) => poke.pokemon.url);
+  
+  // Hacer solicitudes para obtener detalles de cada Pokémon
+  await Promise.all(
+    pokemonUrls.map(async (url) => {
+      // No hay try...catch aquí, los errores serán manejados por el código circundante
+      let pokemonResponse = await axios.get(url);
+      let pokemonData = pokemonResponse.data;
+      
+      // Agregar el Pokémon al array nuevo
+      nuevo.value.push({
+        id: pokemonData.id,
+        img: pokemonData.sprites.other["official-artwork"].front_default,
+        nombre: pokemonData.name,
+        altura: pokemonData.height,
+        peso: pokemonData.weight,
+        tipos: pokemonData.types.map((tipo) => tipo.type.name),
+        estadisticas: pokemonData.stats.map((stat) => {
+          return { name: stat.stat.name, cant: stat.base_stat };
+        }),
+      });
+    })
+  );
+  
+  componenteNuevo.value = true;
+}
+
+
+const radio=ref()
+  function quitarFiltro() {
+  nuevo.value = [];
+  opcionSeleccionada.value = "";
+  radio.value = false
+  componenteNuevo.value = false;
+}
+
 
 </script>
 
@@ -161,6 +245,7 @@ function buscar(){
     width: 120px;
     height: 50px;
   }
+
 
   h1 {
     text-align: center;
@@ -178,12 +263,13 @@ function buscar(){
   .img-fluid{
     width: 570px;
     height: 550px;
-    margin-left: 30%;
+    margin-left: 250px;
   }
 
   .card-img-top{
     background-color: rgb(229, 229, 229);
   } 
+
   .card {
     width: 100%;
     background-color: #fff;
@@ -202,34 +288,49 @@ function buscar(){
   }
 
   .nombre-modal{
-    font-size: 55px;
+    font-size: 60px;
     margin-top: 1%;
-    
   }
 
   .id-modal{
-    font-size: 55px;
+    font-size: 95px;
     margin-right: 90%;
     margin-top: 1%;
     margin-bottom: 1%;
+    color: #A569BD;
   }
 
   .igual-modal{
     font-size: 25px;
   }
 
+  .lado{
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: flex-end
+  }
+
+  .lado1{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 25px;
+    margin-left: 20px;
+  }
+
+
   .bold-text {
     font-weight: bold;
-    font-size: 25px;
+    font-size: 30px;
   }
 
   .modal-dialog{
-    max-width: 80%;
-    width: 80%;
+    max-width: 60%;
+    width: 60%;
   }
 
   #tipo-modal{
-    font-size: 120%;
+    font-size: 140%;
     
   }
   .modal button.btn-close {
@@ -253,8 +354,11 @@ function buscar(){
     margin-bottom: 5px;
   }
 
+  .progress{
+    height: 30px;
+  }
+
   .progress-bar {
-  height: 900%; 
   font-size: 16px;
   }
   
@@ -270,6 +374,11 @@ function buscar(){
     height: 60px;
   }
 
+  #logo{
+    height: 90px;
+    width: 250px;
+  }
+
   .logo {
   margin-right: 20px;
   }
@@ -281,17 +390,19 @@ function buscar(){
     color: black;
     border: 1px solid black;
     border-radius: 400px;
-    width: 450px;
-    height: 38px;
+    width: 480px;
+    height: 50px;
+    margin-right: 10px;
   }
 
   #boton {
     background-color: #007bff;
     border: 1px solid black;
-    padding: 8px 16px;
+    padding: 10px 20px;
     margin-left:30px ;
     border-radius: 400px;
     font-weight: bold;
+    margin-right: 150px;
   }
 
   #filtro {
@@ -305,7 +416,7 @@ function buscar(){
   }
 
   .tipo{
-    width: 70px;
+    width: 90px;
     border-radius: 400px;
     text-align: center;
     display: flex;
@@ -320,27 +431,29 @@ function buscar(){
   color: black;
  }
 
-
  .card-body {
     background-color: #f7f7f7;
     padding: 20px;
     border-radius: 10px;
+    width: 100%;
   }
 
   #opciones{
     display: flex;
     flex-direction: row;
-    margin-right: 600%;
+    width: 55%;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: fit-content;
+    flex-wrap: wrap;
   }
 
   #opcion1{
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
     text-align: center;
-    justify-content: center;
-    padding: 15px;
-  }
-
-  #collapseExample{
-    margin-right: 40px;
   }
 
   #ultimo{
@@ -348,6 +461,53 @@ function buscar(){
     width: 90px;
     font-weight: bold;
     background-color: #9bfae0;
+  }
+
+  .stat-amount{
+    font-size: 20px;
+    margin-left: 10px;
+  }
+
+  .quitar{
+    height: 40px;
+    width: 80px;
+    margin-left: 10px;
+  }
+
+  @media screen and (min-width: 1100px) and (max-width: 1650px) {
+  .img-fluid{
+   margin-left: 10px;
+  }
+  }
+  @media screen and (min-width: 350px) and (max-width: 1000px) {
+  .modal-dialog{
+    max-width: 100%;
+    width: 100%;
+    margin-left: 1px;
+  }
+
+  .arriba{
+    flex-direction: column;
+  }
+
+  #barra1{
+    width:340px;
+    margin-bottom: 10px;
+  }
+
+  #filtro{
+    margin-top: 50px;
+  }
+
+ .lado{
+  flex-direction: column-reverse;
+ }
+ .img-fluid{
+    width: 570px;
+    height: 550px;
+    margin-left: 10px;
+  }
+
   }
 
 </style>
